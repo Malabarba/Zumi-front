20db163c44ed847723c16021d07235d495aacbdc
Artur Malabarba <artur@endlessparentheses.com>
2018-02-12 21:10:00 -0200

    btn

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App'
 import router from './router.js'
 import AppIcon from './components/AppIcon'
 import AppFooter from './components/AppFooter'
+import Btn from './components/Btn'
 
 Vue.config.productionTip = false
 
 Vue.component('f-a', AppIcon)
 Vue.component('app-footer', AppFooter)
+Vue.component('btn', Btn)
 
 /* eslint-disable no-unused-vars */
 const app = new Vue({
