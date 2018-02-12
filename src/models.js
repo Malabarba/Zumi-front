4edaf1b4417ac8bd60f1ed49c802bcea4d47af2f
Artur Malabarba <artur@endlessparentheses.com>
2018-02-12 01:57:10 -0200

    Safer model.js

--- src/models.js
+++ src/models.js
@@ -19,8 +19,20 @@ export class Property {
     this.lot_size_m2 = lot_size_m2
     this.usable_size_m2 = usable_size_m2
     this.condo_cost_cents = condo_cost_cents
-    this.condo_cost = new Money(condo_cost_cents / 100)
+    if (condo_cost_cents) this.condo_cost = new Money(condo_cost_cents / 100)
     this.floor = floor
+
+    if (this.toilet_count && this.bath_count) {
+      const toilet = this.toilet_count - this.bath_count
+      const toiletText = toilet > 0 ? ` + ${toilet}` : ''
+      this.descList = [
+        new PropertyDetail(this.bedroom_count.toString(), 'Quartos', 'bed'),
+        new PropertyDetail(`${this.bath_count}${toiletText}`, 'Banheiros', 'bath'),
+        new PropertyDetail(`${this.floor}º`, 'Andar', 'building'),
+        new PropertyDetail(`${this.usable_size_m2}m²`, 'Área útil', 'crop'),
+        new PropertyDetail(`${this.lot_size_m2}m²`, 'Área total', 'crop')
+      ]
+    }
   }
 }
 Property.singular = 'property'
@@ -43,20 +55,14 @@ export class Listing {
     this.uniq_hash = uniq_hash
     this.property = new Property(property)
     this.price_cents = price_cents
-    this.price = new Money(price_cents / 100)
+    if (price_cents) this.price = new Money(price_cents / 100)
     this.furnished = furnished
     this.published_at = published_at
     this.deleted_at = deleted_at
+  }
 
-    const toilet = this.property.toilet_count - this.property.bath_count
-    const toiletText = toilet > 0 ? ` + ${toilet}` : ''
-    this.descList = [
-      new PropertyDetail(this.property.bedroom_count.toString(), 'Quartos', 'bed'),
-      new PropertyDetail(`${this.property.bath_count}${toiletText}`, 'Banheiros', 'bath'),
-      new PropertyDetail(`${this.property.floor}º`, 'Andar', 'building'),
-      new PropertyDetail(`${this.property.usable_size_m2}m²`, 'Área útil', 'crop'),
-      new PropertyDetail(`${this.property.lot_size_m2}m²`, 'Área total', 'crop')
-    ]
+  get descList() {
+    return this.property && this.property.descList
   }
 }
 Listing.singular = 'listing'
@@ -78,8 +84,8 @@ export class User {
     this.birth_date = birth_date
     this.buy_visits = buy_visits
     this.sale_visits = sale_visits
-    this.properties = properties.map(Property.from)
-    this.favorite_listings = favorite_listings.map(Listing.from)
+    this.properties = properties && properties.map(Property.from)
+    this.favorite_listings = favorite_listings && favorite_listings.map(Listing.from)
   }
 }
 User.singular = 'user'
