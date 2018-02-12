11b461b7a318d4e0121530c6fd6b60633bf1f8bd
Artur Malabarba <artur@endlessparentheses.com>
2018-02-12 21:09:29 -0200

    Btn can be disabled

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,12 +1,23 @@
 <template>
-  <button type="button" class="button" :class="['is-' + as]">{{l}}</button>
+  <button type="button" class="button" :class="classBinding"
+          :disabled="disabled"
+          @click.prevent="$emit('click', $event)">{{l}}</button>
 </template>
 
 <script>
 export default {
   props: {
     as: { required: false, type: String, default: 'link' },
-    l: { required: true, type: String }
+    l: { required: true, type: String },
+    disabled: { required: false, type: Boolean, default: false },
+  },
+
+  computed: {
+    classBinding() {
+      const binding = { 'is-disabled': this.disabled }
+      binding['is-' + this.as] = true
+      return binding
+    }
   }
 }
 </script>
