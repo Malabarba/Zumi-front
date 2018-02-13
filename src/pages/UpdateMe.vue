7e32fee4a7a2f1efa138572905116437575f78e3
Artur Malabarba <artur@endlessparentheses.com>
2018-02-13 20:46:35 -0200

    Define UpdateMe.vue page

---
+++ src/pages/UpdateMe.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section class="section container" id="update-me">
+      <div class="columns">
+        <article class="column is-4">
+          <p>Olá {{me.first_name}}, aqui você pode atualizar os seus
+            dados.</p>
+          <p>Se quiser trocar sua senha,
+            <router-link to="/troca-de-senha">clique aqui</router-link></p>
+          <p>Qualquer dúvida, entre em contato conosco em
+            <a href="mailto:contato@zumi.com.br">contato@zumi.com.br</a>.</p>
+        </article>
+        <article class="column is-8">
+          <user-form mode="update"/>
+        </article>
+      </div>
+    </section>
+
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+import Api from '@/api'
+import { buildMeta } from './meta.service'
+import UserForm from '@/components/UserForm'
+
+export default {
+  head: buildMeta({title: 'Atualizar Cadastro',
+                   description: 'Atualize seu dados cadastrais na Zumi.'}),
+  components: {UserForm},
+  data() {
+    return {
+      me: Api.me.state
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import HowItWorks from '@/pages/HowItWorks'
 import ListingIndex from '@/pages/ListingIndex'
 import ListingShow from '@/pages/ListingShow'
 import Register from '@/pages/Register'
+import UpdateMe from '@/pages/UpdateMe'
 
 Vue.use(VueHead)
 Vue.use(Router)
@@ -15,6 +16,8 @@ export default new Router({
   routes: [
     {path: '/', name: 'Home', component: Home},
     {path: '/cadastro', name: 'Register', component: Register},
+    {path: '/meu-cadastro', name: 'UpdateMe', component: UpdateMe},
+    {path: '/troca-de-senha', name: 'UpdateMe', component: UpdateMe},
     {path: '/como-funciona', name: 'HowItWorks', component: HowItWorks},
     {path: '/imoveis', props: {hasMap: false}, name: 'ListingIndex', component: ListingIndex},
     {path: '/imoveis-no-mapa', props: {hasMap: true}, name: 'ListingMap', component: ListingIndex},
