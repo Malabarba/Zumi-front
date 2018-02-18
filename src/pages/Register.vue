bed1cbef7c795bcbefa9fa8e1369836f464deb64
Artur Malabarba <artur@endlessparentheses.com>
2018-02-18 15:50:28 -0300

    Add login button to register page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -4,9 +4,9 @@
       <div class="columns">
         <article class="column is-4">
           <p>Preenchendo este cadastro você poderá comprar e vender
-            imóveis na Zumi.</p>
-          <p>Qualquer dúvida, entre em contato conosco em
-            <a href="mailto:contato@zumi.com.br">contato@zumi.com.br</a>.</p>
+            imóveis na Zumi. Qualquer dúvida, entre em contato conosco
+            em <a href="mailto:contato@zumi.com.br">contato@zumi.com.br</a>.</p>
+          <div>Já tem um cadastro? <a href="#login" @click.prevent="openLogin">Faça Login.</a></div>
         </article>
         <article class="column is-8">
           <user-form mode="create"/>
@@ -21,6 +21,7 @@
 <script>
 import { buildMeta } from './meta.service'
 import UserForm from '@/components/UserForm'
+import EventBus from '@/event-bus'
 // import { User } from '@/models'
 // import Api from '@/api'
 
@@ -28,6 +29,15 @@ export default {
   name: 'Register',
   head: buildMeta({title: 'Cadastro',
                    description: 'Crie seu cadastro de comprador ou vendedor na Zumi'}),
-  components: {UserForm}
+  components: {UserForm},
+  methods: {
+    openLogin() { EventBus.$emit('login-modal') }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+article > div {
+  margin-top: 1rem;
+}
+</style>
