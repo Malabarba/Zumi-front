ee223925e1074a79928bd1c8e56c78214db8d640
Artur Malabarba <artur@endlessparentheses.com>
2018-02-18 16:28:51 -0300

    Reorganize api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import EventBus from '@/event-bus'
 const baseUrl = '/api/v1'
 
 function logIt(x) {
-  console.log(x)
+  console.log('[Received]', x)
   return x
 }
 
@@ -19,24 +19,10 @@ const del = (...args) => request('delete', ...args)
 const post = (...args) => request('post', ...args).then(x => x && x.data)
 const patch = (...args) => request('patch', ...args).then(x => x && x.data)
 
-// if (state[path]) fn(state[path])
-//   .then(response => state[path] = response)
-//   .then(fn)
-
 function makeApi(model, api) {
   if (!api.state) api.state = {}
-  api.promises = []
-  const eventKey = `api-${api.singular}-state`
 
-  const trackPromise = function (p) {
-    api.promises.push(p)
-    EventBus.$emit(eventKey, api.pending())
-    return p.finally(() => {
-      api.promises = api.promises.filter(x => x !== p)
-      EventBus.$emit(eventKey, api.pending())
-    })
-  }
-
+  // Caching common methods
   const castAndCache = function(json) {
     const instance = model.from(json || {})
     api.cache(instance)
@@ -47,7 +33,7 @@ function makeApi(model, api) {
   const index = api.index
   if (index) {
     api.indexRaw = index
-    api.index = (...args) => trackPromise(index(...args).then(data => data[plural].map(castAndCache)))
+    api.index = (...args) => index(...args).then(data => data[plural].map(castAndCache))
   }
 
   const singular = model.singular
@@ -56,11 +42,34 @@ function makeApi(model, api) {
     const m = api[name]
     if (!m) break
     api[`${name}Raw`] = m
-    api[name] = (...args) => trackPromise(m(...args).then(x => castAndCache(x[singular])))
+    api[name] = (...args) => m(...args).then(x => castAndCache(x[singular]))
+  }
+
+  // Tracking promises and pending status
+  const eventKey = `api-${api.singular}-state`
+  api.promises = []
+
+  const trackPromise = function(p) {
+    api.promises.push(p)
+    EventBus.$emit(eventKey, api.pending())
+    return p.finally(() => {
+      api.promises = api.promises.filter(x => x !== p)
+      EventBus.$emit(eventKey, api.pending())
+    })
+  }
+
+  for (name of api.promiseMehods) {
+    const m = api[name]
+    if (!m) throw `API Method declared but not defined: ${name}`
+    api[name] = (...args) => trackPromise(m(...args))
   }
 
   api.pending = () => api.promises.length > 0
-  api.queue = function(fn) {
+  api.queue = function(f) {
+    const fn = function(...args) {
+      // console.log('queued: ', api.state, api.state.email, api.loggedIn, api.promises);
+      return f(...args)
+    }
     if (api.pending()) api.promises[api.promises.length - 1].finally(fn)
     else fn()
   }
@@ -76,6 +85,8 @@ function makeApi(model, api) {
 export const listing = makeApi(Listing, {
   // cache(l) { this.state[`/listing/${l.uniq_hash}`] = l },
   cache() {},
+
+  promiseMethods: ['index', 'show', 'neighborhoods'],
   index: (params) => get('/listings', { params }),
   show: (id) => get(`/listing/${id}`),
   neighborhoods: () => get('/listings/neighborhoods')
@@ -85,6 +96,8 @@ export const me = makeApi(User, {
   state: new User(),
   get loggedIn() { return !!this.state.email },
   cache(me) { Object.assign(this.state, me) },
+
+  promiseMethods: ['show', 'create', 'update', 'updatePassword', 'login', 'logout'],
   show: () => get('/me'),
   create: (user) => post('/me', { user }),
   update: (user) => patch('/me', { user }),
